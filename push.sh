e962e4da696a98ab43f99f3552223f217c2c0bd4
tmd <ttz.ztt@outlook.com>
2022-12-23 22:24:40 +0800

    tmd

---
+++ push.sh
@@ -0,0 +1,10 @@
+###
+ # @Description  : 
+ # @Autor        : TMD
+ # @Date         : 2022-12-23 22:24:31
+ # @LastEditTime : 2022-12-23 22:24:32
+### 
+#!/bin/bash
+git add .
+git commit -m "tmd"
+git push
